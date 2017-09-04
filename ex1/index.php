315a44b28f7a637ec925b874d49cf891513ce494
lburns23 <31327767+lburns23@users.noreply.github.com>
2017-09-03 22:49:42 -0400

    Update 2
    Put in more code and fixed things the code.

--- ex1/index.php
+++ ex1/index.php
@@ -4,8 +4,8 @@
 
 <?php include 'header.php';?>
 <h1><?php echo "$myName" ;?> </h1>
-<ul><?php $x=1 
-while($x => 1) {echo $myArray; $x++ ;?></ul>
+<ul><?php  function my_function () {$x=1 
+while($x => 1) {echo $myArray}; $x++ ;} my_function();?></ul>
 <?php include 'footer.php';?>
 <?php
 $myArray = array ('myName'  => 'Laura Burns',
