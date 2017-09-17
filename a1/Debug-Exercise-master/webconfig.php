e6043a21e435db8294b6fa0c351f0b6f9b23ad08
lburns23 <31327767+lburns23@users.noreply.github.com>
2017-09-17 17:33:23 -0400

    Changed the Webconfig
    Changed the URL root and now it works on my phone.

--- a1/Debug-Exercise-master/webconfig.php
+++ a1/Debug-Exercise-master/webconfig.php
@@ -1,4 +1,4 @@
 <?php
 
 define('ABSOLUTE_PATH', '/home/burns23/htdocs/a1/Debug-Exercise-master/');
-define('URL_ROOT', '\a1\Debug-Exercise-master');
+define('URL_ROOT', 'http://corsair.cs.iupui.edu:21851/a1/Debug-Exercise-master');
